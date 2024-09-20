e5b0ea621f12c8ae7dd789fd3ef203527c07a56a
Darragh Mulhall <darraghmulhall@Darraghs-MacBook-Pro.local>
2024-09-20 08:00:52 -0700

    added file writer to json_paths

--- src/json_paths.py
+++ src/json_paths.py
@@ -1,9 +1,13 @@
 
-import find_paths
+import src.find_paths as find_paths
 import json
 
-def query_paths():
-    json_input = '{"queries": [{"paths": {"start": "a","end": "e" }},{"cheapest": {"start": "a","end": "e" }}, {"cheapest": {"start": "a","end": "h" }}]}'
+JSON_FILE = 'json/json_paths_result.json'
+
+
+def query_paths(json_input=None):
+    if json_input is None:
+        json_input = input("Please insert json:")
     json_data = json.loads(json_input)
 
     queries = json_data.get('queries', [])
@@ -19,4 +23,9 @@ def query_paths():
         if len(search_result) == 0 and key_type == 'cheapest':
             search_result = False
         answers.append({key_type:{'from':start, 'to':end, result_path_type:search_result}})
-    return json.dumps({'answers':answers})
+    return {'answers':answers}
+    
+
+def write_json(json_result):
+    with open (JSON_FILE, 'w') as file:
+        json.dump(json_result, file)
