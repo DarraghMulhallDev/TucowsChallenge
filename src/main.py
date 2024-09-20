c276ed16b03624080aacd45cffa5d5982833de86
Darragh Mulhall <darraghmulhall@Darraghs-MacBook-Pro.local>
2024-09-19 22:53:18 -0700

    update to main to house function for xml challenge and call it

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import xml_parser
 
-xml_parser.download_file(xml_parser.XML_URL)
-parsed_xml = xml_parser.parse_graph_xml(xml_parser.GRAPH_FILE)
+def xml_challenge():
+    xml_parser.download_file(xml_parser.XML_URL)
+    parsed_xml = xml_parser.parse_graph_xml(xml_parser.GRAPH_FILE)
+    return parsed_xml
+
+parsed_xml = xml_challenge()
