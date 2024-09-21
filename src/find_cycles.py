113cdfef03b60ab26e4566295eef4b2da5bf0655
DarraghMulhallDev <darraghmulhall.dev@gmail.com>
2024-09-21 14:35:43 -0700

    Update find_cycles.py - irrelevant comment

--- src/find_cycles.py
+++ src/find_cycles.py
@@ -4,7 +4,6 @@ import database.db_connector as connector
 def find_cycles():
     conn = connector.get_db_connection()
     cur = conn.cursor()
-    # run create tables script
     cur.execute('SELECT * FROM find_cycles(1);')
     conn.commit()
     results = cur.fetchall()
