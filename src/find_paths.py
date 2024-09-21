e21b45952d311cab8a91cc515d7b0a5d920bbe61
DarraghMulhallDev <darraghmulhall.dev@gmail.com>
2024-09-21 14:36:55 -0700

    Update find_paths.py - added comment for func

--- src/find_paths.py
+++ src/find_paths.py
@@ -1,10 +1,10 @@
 import database.db_connector as connector
 
+# make a call to finds_paths or find_cheapest_path sql functions depending on input, passing the start and end nodes
 def find_paths_func(start, end, cheapest:bool):
     conn = connector.get_db_connection()
     cur = conn.cursor()
     function = 'find_paths' if not cheapest else 'find_cheapest_path'
-    # run create tables script
     cur.execute('SELECT * FROM {0}(1,{1},{2});'.format(function, start, end))
     conn.commit()
     results = cur.fetchall()
